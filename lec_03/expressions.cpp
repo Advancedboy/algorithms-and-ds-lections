8eb21b4f10dc74b4d0ca81f8981f57ad5f82af9a
Haikevich <51019643+Advancedboy@users.noreply.github.com>
2023-07-27 21:01:33 +0300

    Added expressions.cpp file

---
+++ lec_03/expressions.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+
+double f(double);
+
+int main(){
+    using namespace std;
+    double x;
+    cin >> x;
+    cout << f(x) << endl;
+
+    double s = 0;
+    double factor;
+
+    for(int n = 0; n < 10; n++, factor *= n) {
+        s += pow(-1, n) * pow(x, 2 * n + 1) / factor;
+        factor *= n * 2 * (2 * n + 1);
+    }
+
+    cout << s <<  endl;
+
+    return 0;
+}
+
+double f(double x){
+    return 2 * sin(x);
+}
